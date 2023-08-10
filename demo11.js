3b670f71c8c24ba1ec278d900409beeb9ba0719c
Kumelhaider <131907189+Kumelhaider@users.noreply.github.com>
2023-08-10 22:53:53 +0500

    Add files via upload

---
+++ demo11.js
@@ -0,0 +1,290 @@
+const data = {
+    "statusCode": 0,
+    "succeeded": true,
+    "message": "List of MstCoas",
+    "errors": null,
+    "data": [
+        {
+            "id": 1,
+            "accountTypeId": 1,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "1",
+            "accountName": "Assets-M",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 2,
+            "accountTypeId": 2,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "2",
+            "accountName": "Liabilities\r\n-M",
+            "accountTypeName": "Liabilities\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 3,
+            "accountTypeId": 3,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "3",
+            "accountName": "Equity\r\n-M",
+            "accountTypeName": "Equity\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 4,
+            "accountTypeId": 4,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "4",
+            "accountName": "Revenues\r\n-M",
+            "accountTypeName": "Revenues\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 5,
+            "accountTypeId": 5,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "5",
+            "accountName": "Non Operating Revenues\r\n-M",
+            "accountTypeName": "Non Operating Revenues\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 6,
+            "accountTypeId": 6,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "6",
+            "accountName": "Cost Of Sales\r\n-M",
+            "accountTypeName": "Cost Of Sales\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 7,
+            "accountTypeId": 7,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "7",
+            "accountName": "Operating Expenses\r\n-M",
+            "accountTypeName": "Operating Expenses\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 8,
+            "accountTypeId": 8,
+            "accountLevel": 1,
+            "parentAccountKey": 0,
+            "sameLevelAccountKey": 0,
+            "accountCode": "8",
+            "accountName": "Non Operating Expenses\r\n-M",
+            "accountTypeName": "Non Operating Expenses\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": null,
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 10,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 0,
+            "accountCode": "qbss111",
+            "accountName": "plak",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 11,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 0,
+            "accountCode": "5678",
+            "accountName": "Current Assets",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 12,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 0,
+            "accountCode": "Ac11",
+            "accountName": "An 11",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 13,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 10,
+            "accountCode": "Ac 12",
+            "accountName": "An 12",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": "plak"
+        },
+        {
+            "id": 14,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 10,
+            "accountCode": "ac 13",
+            "accountName": "an 13",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": "plak"
+        },
+        {
+            "id": 15,
+            "accountTypeId": 1,
+            "accountLevel": 2,
+            "parentAccountKey": 1,
+            "sameLevelAccountKey": 10,
+            "accountCode": "ac 14",
+            "accountName": "an 14",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Assets-M",
+            "sameLevelAccountName": "plak"
+        },
+        {
+            "id": 16,
+            "accountTypeId": 1,
+            "accountLevel": 3,
+            "parentAccountKey": 10,
+            "sameLevelAccountKey": null,
+            "accountCode": "3ac 15",
+            "accountName": "3an 15",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Title",
+            "parentAccountName": "plak",
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 17,
+            "accountTypeId": 1,
+            "accountLevel": 3,
+            "parentAccountKey": 10,
+            "sameLevelAccountKey": 16,
+            "accountCode": "3ac 16",
+            "accountName": "3an 16",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Title",
+            "parentAccountName": "plak",
+            "sameLevelAccountName": "3an 15"
+        },
+        {
+            "id": 18,
+            "accountTypeId": 1,
+            "accountLevel": 3,
+            "parentAccountKey": 10,
+            "sameLevelAccountKey": 17,
+            "accountCode": "3ac 17",
+            "accountName": "3an 17",
+            "accountTypeName": "Assets",
+            "accountLevelName": "Title",
+            "parentAccountName": "plak",
+            "sameLevelAccountName": "3an 16"
+        },
+        {
+            "id": 19,
+            "accountTypeId": 2,
+            "accountLevel": 2,
+            "parentAccountKey": 2,
+            "sameLevelAccountKey": null,
+            "accountCode": "L ac 11",
+            "accountName": "L an 11",
+            "accountTypeName": "Liabilities\r\n",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Liabilities\r\n-M",
+            "sameLevelAccountName": null
+        },
+        {
+            "id": 20,
+            "accountTypeId": 2,
+            "accountLevel": 2,
+            "parentAccountKey": 2,
+            "sameLevelAccountKey": 19,
+            "accountCode": "L ac 12",
+            "accountName": "L an 12",
+            "accountTypeName": "Liabilities\r\n",
+            "accountLevelName": "Transaction",
+            "parentAccountName": "Liabilities\r\n-M",
+            "sameLevelAccountName": "L an 11"
+        },
+        {
+            "id": 21,
+            "accountTypeId": 2,
+            "accountLevel": 3,
+            "parentAccountKey": 19,
+            "sameLevelAccountKey": null,
+            "accountCode": "L ac 13",
+            "accountName": "L an 13",
+            "accountTypeName": "Liabilities\r\n",
+            "accountLevelName": "Title",
+            "parentAccountName": "L an 11",
+            "sameLevelAccountName": null
+        }
+    ]
+};
+
+var new_data = data.data;
+
+const accountMap = new Map();
+
+// Create a map of account IDs to their corresponding objects
+new_data.forEach((account) => {
+account.children = [];
+accountMap.set(account.id, account);
+});
+
+// Build the hierarchy by assigning children to their parent accounts
+new_data.forEach((account) => {
+const parentAccount = accountMap.get(account.parentAccountKey);
+if (parentAccount) {
+parentAccount.children.push(account);
+}
+});
+
+// Filter out accounts that are not at the top level (parentAccountKey is not 0)
+const accountHierarchy = new_data.filter((account) => account.parentAccountKey === 0);
+console.log(JSON.stringify(accountHierarchy));
